5e461f2d538c792afb238a132a16114943657f95
paulina-polechonska <p.polechonska@gmail.com>
2023-04-16 23:41:23 +0200

    header

---
+++ src/js/Header.js
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import ConstructionIcon from '@mui/icons-material/Construction';
+import theme from "../theme/theme";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+
+const Header = () => {
+
+    const pages = ['Pulpit', 'Nowe zlecenie'];
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
+    return (
+        <ThemeProvider theme={theme}>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <ConstructionIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }} color={'secondary'}/>
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                textDecoration: 'none',
+                            }}
+                            color={'secondary'}
+                        >
+                            DAMCAR
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }}}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none'},
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <ConstructionIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} color={'secondary'} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                textDecoration: 'none',
+                            }}
+                            color={'secondary'}
+                        >
+                            DAMCAR
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end'}}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block', pr:'30px' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    );
+};
+
+export default Header;
+
+
+
---
+++ src/theme/theme.js
@@ -0,0 +1,18 @@
+import {createTheme} from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#46484A',
+        },
+        secondary: {
+            main: '#e16030',
+        },
+        third: {
+            main: '#f5f5f6',
+        },
+
+    },
+});
+
+export default theme;
