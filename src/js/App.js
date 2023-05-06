180a04d487dfc54b85ec83ca39c9c655c6dcd49b
paulina-polechonska <p.polechonska@gmail.com>
2023-05-06 13:26:12 +0200

    fix

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Header from "./Header";
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
+import {Route, Routes, BrowserRouter as Router, HashRouter} from 'react-router-dom';
 import AddTask from "./AddTask";
 import Login from "./Login";
 import Desktop from "./Desktop";
@@ -12,17 +12,18 @@ const App = () => {
   const [name, setName] = useState('Login');
 
   return (
-       <Router basename='/damcar-management'>
+       // <HashRouter basename='/damcar-management'>
+      <HashRouter>
           <UserContext.Provider value={{name, setName}}>
               <Header usernameH={name}/>
               <Routes>
-                  <Route exact path='/' element={< Login />}/>
+                  <Route path='/' element={< Login />}/>
                   <Route path='/Pulpit' element={< Desktop />}/>
                   <Route path='/Dodaj' element={< AddTask />}/>
                   <Route path='/:id' element={< UpdateTask />}/>
               </Routes>
           </UserContext.Provider>
-      </Router>
+      </HashRouter>
   );
 }
 
